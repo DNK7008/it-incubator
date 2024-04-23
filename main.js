621ddec938b6558d6e1e7d0aff2bf993dc9f762b
dnk7008 <gorodenskiydenis91@gmail.com>
2024-04-23 17:53:39 +0300

    add fit

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 // Местонахождение корабля
-const location1 = 3;
-const location2 = 4;
-const location3 = 5;
+let locationRandom = Math.floor(Math.random() * 5)  // от 0 до 4
+const location1 = locationRandom;
+const location2 = locationRandom + 1;
+const location3 = locationRandom + 2;
 
 // Предпологаемая позиция корабля(выстрел)
 let guess;
-
 // Условия завершения игры (потоплен или нет)
 let isSunk = false;
-
 // Добавляем счетчик попаданий
 let hits = 0;
-
 while (isSunk === false) {
   guess = +prompt("Введите предположительное местонахождение корабля, цифры от 0 до 6")
-  if (guess === location1 || guess === location2 || guess === location3) {
-    alert("HIT!!!");
-
-    // считаем попадания
-    hits = hits + 1;
-    if (hits === 3) {
-      isSunk = true;
-      alert("You sunk my battle ship!");
-    }
+  if (guess < 0 || guess > 6) {
+    alert("Please, enter a valid cell number")
   } else {
-    alert("MISS!!!");
+    if (guess === location1 || guess === location2 || guess === location3) {
+      alert("HIT!!!");
+      // считаем попадания
+      hits = hits + 1;
+      if (hits === 3) {
+        isSunk = true;
+        alert("You sunk my battle ship!");
+      }
+    } else {
+      alert("MISS!!!");
+    }
   }
+
 }
 
