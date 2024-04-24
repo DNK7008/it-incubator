18563b0b11afd3035fc134fbc1077ac6623bddcc
Denis Soroko <gorodenskiydenis91@gmail.com>
2024-04-24 11:13:49 +0300

    add v1.1

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const location3 = locationRandom + 2;
 
 // Предпологаемая позиция корабля(выстрел)
 let guess;
+// Счетчик выстрелов
+let guesses = 0;
 // Условия завершения игры (потоплен или нет)
 let isSunk = false;
 // Добавляем счетчик попаданий
@@ -15,10 +17,12 @@ while (isSunk === false) {
   if (guess < 0 || guess > 6) {
     alert("Please, enter a valid cell number")
   } else {
+    // счетчик выстрелов
+    guesses++;
     if (guess === location1 || guess === location2 || guess === location3) {
       alert("HIT!!!");
       // считаем попадания
-      hits = hits + 1;
+      hits++;
       if (hits === 3) {
         isSunk = true;
         alert("You sunk my battle ship!");
@@ -27,6 +31,8 @@ while (isSunk === false) {
       alert("MISS!!!");
     }
   }
-
+  
 }
 
+const stats = "You have " + hits + " hits out of " + guesses + " shots.";
+alert(stats);
