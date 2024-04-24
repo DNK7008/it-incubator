c3d2dbc0597f16b446d04bbe432a21b5130864f7
Denis Soroko <gorodenskiydenis91@gmail.com>
2024-04-24 19:07:41 +0300

    save progress

---
+++ generatorRandomWords/main.js
@@ -0,0 +1,12 @@
+const words_1 = ["Мой кот", "Моя собака", "Мой попугай"];
+const words_2 = ["любит есть морковку", "хочет погрызть", "всегда ищет"];
+const words_3 = ["морковку", "макороны", "косточку"];
+
+function wordsGenerator(wordsArray) {
+  let indexRandom = Math.floor(Math.random() * wordsArray.lenght);
+  
+}
+
+
+// let test = Math.floor(Math.random() * words_1.length);
+// console.log(words_1[test]);
