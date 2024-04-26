b30414c45eb4763dce632ef53267736f0159ac00
Denis Soroko <gorodenskiydenis91@gmail.com>
2024-04-26 10:59:57 +0300

    add fit

--- generatorRandomWords/main.js
+++ generatorRandomWords/main.js
@@ -1,4 +1,4 @@
-const words_1 = ["Мой кот", "Моя собака", "Мой попугай"];
+const words_1 = ["Ира", "Женя", "Викторович", "Denis", "Юля"];
 const words_2 = ["любит есть", "хочет погрызть", "всегда ищет"];
 const words_3 = ["морковку", "макороны", "косточку"];
 
@@ -6,4 +6,7 @@ function indexGet(wordsArray) {
   let indexRandom = Math.floor(Math.random() * wordsArray.length);
   return indexRandom;
 }
+
+
 alert(words_1[indexGet(words_1)] + " " + words_2[indexGet(words_2)] + " " + words_3[indexGet(words_3)]);
+console.log(words_1[indexGet(words_1)] + " " + words_2[indexGet(words_2)] + " " + words_3[indexGet(words_3)]);
