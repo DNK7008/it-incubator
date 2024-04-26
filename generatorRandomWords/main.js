8ff1591ff1a57dbf02e78607d19e5d1084744b3e
dnk7008 <gorodenskiydenis91@gmail.com>
2024-04-26 11:53:52 +0300

    refactoring

--- generatorRandomWords/main.js
+++ generatorRandomWords/main.js
@@ -2,10 +2,15 @@ const words_1 = ["Ира", "Женя", "Викторович", "Denis", "Юля"
 const words_2 = ["любит есть", "хочет погрызть", "всегда ищет"];
 const words_3 = ["морковку", "макороны", "косточку"];
 
-function indexGet(wordsArray) {
-  let indexRandom = Math.floor(Math.random() * wordsArray.length);
-  return indexRandom;
+function makePhrases(param_1, param_2, param_3) {
+  let result = indexGet(param_1) + " " + indexGet(param_2) + " " + indexGet(param_3);
+  return result;
 }
 
-alert(words_1[indexGet(words_1)] + " " + words_2[indexGet(words_2)] + " " + words_3[indexGet(words_3)]);
-console.log(words_1[indexGet(words_1)] + " " + words_2[indexGet(words_2)] + " " + words_3[indexGet(words_3)]);
+function indexGet(wordsArray) {
+  let indexRandom = Math.floor(Math.random() * wordsArray.length);
+  return wordsArray[indexRandom];
+}
+
+console.log(makePhrases(words_1, words_2, words_3));
+alert(makePhrases(words_1, words_2, words_3));
