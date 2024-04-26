1aa9f0597150fbfa34b64b88cd325ae3cfec86d4
Denis Soroko <gorodenskiydenis91@gmail.com>
2024-04-26 10:19:13 +0300

    add fit

--- generatorRandomWords/main.js
+++ generatorRandomWords/main.js
@@ -1,12 +1,9 @@
 const words_1 = ["Мой кот", "Моя собака", "Мой попугай"];
-const words_2 = ["любит есть морковку", "хочет погрызть", "всегда ищет"];
+const words_2 = ["любит есть", "хочет погрызть", "всегда ищет"];
 const words_3 = ["морковку", "макороны", "косточку"];
 
-function wordsGenerator(wordsArray) {
-  let indexRandom = Math.floor(Math.random() * wordsArray.lenght);
-  
+function indexGet(wordsArray) {
+  let indexRandom = Math.floor(Math.random() * wordsArray.length);
+  return indexRandom;
 }
-
-
-// let test = Math.floor(Math.random() * words_1.length);
-// console.log(words_1[test]);
+alert(words_1[indexGet(words_1)] + " " + words_2[indexGet(words_2)] + " " + words_3[indexGet(words_3)]);
