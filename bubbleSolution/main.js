e55461d425f81f7800580a49b8f7acc20e5ec324
dnk7008 <gorodenskiydenis91@gmail.com>
2024-05-20 17:44:46 +0300

    add task

--- bubbleSolution/main.js
+++ bubbleSolution/main.js
@@ -1,21 +1,34 @@
 let bubble = [];
 let maxScore = 0;
 let minScore = 100;
+let bestSolutions = []; 
+let minSolutions = []; 
+// создаем массив произвольных значений
 for (let i = 0; i < 36; i++) {
-  bubble.push(Math.floor(Math.random() * 100))
+  bubble.push(Math.floor(Math.random() * 20));
+  // вывод значений массива в консоль 
   console.log("Babble solution #" + i + " score: " + bubble[i]);
+  // находим максимальное значение в массиве
   if (maxScore < bubble[i]) {
     maxScore = bubble[i]
   }
-}
-console.log("Babble tests: " + bubble.length);
-console.log("Highest bubble score: " + maxScore);
-
-// находим самое минимальное значение и выводим его
-for (let i = 0; i < bubble.length; i++) {
+  // находим минимальное  значение в массиве
   if (bubble[i] < minScore) {
     minScore = bubble[i];
   }
 }
+
+console.log("Babble tests: " + bubble.length);
+console.log("Highest bubble score: " + maxScore);
 console.log("Min bubble score: " + minScore);
 
+for (let i = 0; i < bubble.length; i++) {
+  if (bubble[i] === maxScore) {
+    bestSolutions.push(i);
+  }
+  if (bubble[i] === minScore) {
+    minSolutions.push(i);
+  }
+} 
+console.log("Best bubble score: " + bestSolutions);
+console.log("Min bubble score: " + minSolutions);
